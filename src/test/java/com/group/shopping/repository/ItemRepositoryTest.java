d00360918d17d3f4433135ae8382b68a4810890f
Beginner <dkstmdwn17@naver.com>
2023-03-29 21:37:12 +0900

    test: Querydsl을 이용해 조회 테스트
    itemSellStatus가 SELL이고,
    itemDetail을 입력해서
    Id 순으로 내림차순 한 결과 반환.
    Querydsl을 통해 자바 소스코드로
    SQL과 비슷하게 소스를 작성 가능
    fetch()메소드 실행 시점에 쿼리문 실행

--- src/test/java/com/group/shopping/repository/ItemRepositoryTest.java
+++ src/test/java/com/group/shopping/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.group.shopping.repository;
 
 import com.group.shopping.domain.item.Item;
+import com.group.shopping.domain.item.QItem;
 import com.group.shopping.domain.item.constant.ItemSellStatus;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,6 +24,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     public void createItemList() {
         for (int i = 1; i <= 10; i ++) {
@@ -91,6 +99,24 @@ class ItemRepositoryTest {
     public void findByItemDetailByNative() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetailNative("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
         for (Item item : itemList) {
             System.out.println(item.toString());
         }

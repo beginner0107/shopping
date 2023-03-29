5a0294a0d7c3f1e846d7838589ba7337569a4c72
Beginner <dkstmdwn17@naver.com>
2023-03-29 19:32:48 +0900

    feat: 상품명을 가지고 상품을 검색하는 쿼리 메소드 작성
    상품명을 가지고 상품을 검색하는
    쿼리 메소드를 작성함

--- src/main/java/com/group/shopping/repository/ItemRepository.java
+++ src/main/java/com/group/shopping/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.group.shopping.repository;
 import com.group.shopping.domain.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
 }

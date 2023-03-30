f71e36cfd5470b9a3f7af17e65934e0c09791aec
Beginner <dkstmdwn17@naver.com>
2023-03-30 23:54:37 +0900

    feat: 회원 Repository와 Service 계층 생성
    Repository에는 중복회원을 확인하는
    findByEmail 메서드 생성.
    Service 계층은 아직 기능 구현 X

---
+++ src/main/java/com/group/shopping/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.group.shopping.repository;
+
+import com.group.shopping.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
---
+++ src/main/java/com/group/shopping/service/member/MemberService.java
@@ -0,0 +1,16 @@
+package com.group.shopping.service.member;
+
+import com.group.shopping.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    
+}

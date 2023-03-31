90be678e7ef1f4167a9e282df625ad024d782bf2
Beginner <dkstmdwn17@naver.com>
2023-03-31 16:54:52 +0900

    test: 로그인, 로그인 실패 테스트
    로그인, 로그인 실패 테스트

--- src/test/java/com/group/shopping/web/MemberControllerTest.java
+++ src/test/java/com/group/shopping/web/MemberControllerTest.java
@@ -1,15 +1,23 @@
 package com.group.shopping.web;
 
+import com.group.shopping.domain.member.Member;
 import com.group.shopping.dto.member.MemberFormDto;
+import com.group.shopping.service.member.MemberService;
 import com.group.shopping.util.FormDataEncoder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -17,10 +25,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("회원 관련 컨트롤러")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
+@Transactional
 class MemberControllerTest {
 
     @Autowired private MockMvc mvc;
     @Autowired private FormDataEncoder formDataEncoder;
+
+    @Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired private MemberService memberService;
 
     @DisplayName("[view][GET] 회원가입 페이지")
     @Test
@@ -50,5 +64,41 @@ class MemberControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"))
                 .andExpect(redirectedUrl("/"));
+    }
+
+    @DisplayName("[view][POST] 로그인 성공")
+    @Test
+    void givenEmailPassword_whenRequesting_thenLogin() throws Exception {
+        // Given
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+
+        // When & Then
+        mvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+
+    @DisplayName("[view][POST] 로그인 실패")
+    @Test
+    void givenEmailPassword_whenRequesting_thenFailLogin() throws Exception {
+        // Given
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+
+        // When & Then
+        mvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
+    public Member createMember(String email, String password) {
+        MemberFormDto memberFormDto = MemberFormDto.of("홍길동", email, password, "서울시 마포구 합정동");
+        Member member = Member.createMember(memberFormDto, bCryptPasswordEncoder);
+        return memberService.saveMember(member);
     }
 }

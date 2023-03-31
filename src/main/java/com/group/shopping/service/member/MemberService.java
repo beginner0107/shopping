ec6eb0d36ab24eb66e588370f2c84eee0866047b
Beginner <dkstmdwn17@naver.com>
2023-03-31 09:56:51 +0900

    feat: 회원가입 로직 구현
    회원가입 로직을 구현,
    중복된 이메일을 입력했을 경우
    중복된 회원으로 간주하고 예외처리

--- src/main/java/com/group/shopping/service/member/MemberService.java
+++ src/main/java/com/group/shopping/service/member/MemberService.java
@@ -1,9 +1,12 @@
 package com.group.shopping.service.member;
 
+import com.group.shopping.domain.member.Member;
 import com.group.shopping.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -12,5 +15,15 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember.isPresent()) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
 }

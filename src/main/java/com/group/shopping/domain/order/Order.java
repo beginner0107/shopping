eaaf9f5e26c2cb624a248d3469cde17c3e9583ff
Beginner <dkstmdwn17@naver.com>
2023-04-03 11:04:32 +0900

    refactor: Order 엔티티에 컬렉션 지연 로딩 설정 추가
    Order 엔티티에 컬렉션 지연 로딩 설정 추가

--- src/main/java/com/group/shopping/domain/order/Order.java
+++ src/main/java/com/group/shopping/domain/order/Order.java
@@ -30,7 +30,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private final List<OrderItem> orderItems = new ArrayList<>();
 
     @CreatedDate

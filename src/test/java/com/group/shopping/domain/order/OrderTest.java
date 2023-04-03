d9cb77b4a752bbc47a695c0452bdf5aa74dfa2ee
Beginner <dkstmdwn17@naver.com>
2023-04-03 10:58:15 +0900

    test: 고아객체 제거 테스트 진행
    고아객체 제거 테스트

--- src/test/java/com/group/shopping/domain/order/OrderTest.java
+++ src/test/java/com/group/shopping/domain/order/OrderTest.java
@@ -2,7 +2,9 @@ package com.group.shopping.domain.order;
 
 import com.group.shopping.domain.constant.ItemSellStatus;
 import com.group.shopping.domain.item.Item;
+import com.group.shopping.domain.member.Member;
 import com.group.shopping.repository.ItemRepository;
+import com.group.shopping.repository.MemberRepository;
 import com.group.shopping.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,6 +30,9 @@ public class OrderTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -67,5 +72,36 @@ public class OrderTest {
         Order savedOrder = orderRepository.findById(order.getId())
                 .orElseThrow(EntityExistsException::new);
         assertEquals(3, savedOrder.getOrderItems().size());
+    }
+
+    public Order createOrder() {
+        Order order = new Order(); // 하나의 주문
+
+        for (int i = 0; i < 3; i ++) {
+            Item item = createItem(); // 3 개의 상품 저장
+            itemRepository.save(item); // 쿼리 + 3
+            OrderItem orderItem = OrderItem.builder() // 주문 내역에 대해 저장할 엔티티
+                    .item(item)
+                    .count(10)
+                    .orderPrice(1000)
+                    .order(order)
+                    .build();
+            order.getOrderItems().add(orderItem); // 영속성 전이
+        }
+
+        Member member = new Member();
+        memberRepository.save(member); // 쿼리 +1
+
+        order.saveMember(member);
+        orderRepository.save(order); // 쿼리 + 4 -> 총 8개의 insert 쿼리 확인(O)
+        return order;
+    }
+
+    @DisplayName("고아객체 제거 테스트")
+    @Test
+    void orphanRemovalTest() {
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0); // 부모 엔티티와 연관관계가 끊어졌기 때문에 삭제 쿼리문 실행
+        em.flush();
     }
 }
